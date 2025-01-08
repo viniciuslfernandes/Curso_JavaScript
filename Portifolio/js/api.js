bb40191fa69f53d2d69538c38bd1b16cfe71cf34
Vinicius <viniciuslima99@live.com>
2025-01-08 20:27:56 -0300

    passando url

--- Portifolio/js/api.js
+++ Portifolio/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileDate(){
-    const url = ""
+    const url = "https://raw.githubusercontent.com/viniciuslfernandes/Curso_JavaScript/refs/heads/main/Portifolio/data/profile.json?token=GHSAT0AAAAAAC5BANPZO3GVFMLUMAWIVWT4Z37BILQ"
     const fetching = await fetch(url)
     return await fetching.json()
 }
