5ff451760a465db92f52916bf3051f437ef31914
Vinicius <viniciuslima99@live.com>
2025-01-09 20:46:58 -0300

    ajustando erro do update

--- Portifolio/js/main.js
+++ Portifolio/js/main.js
@@ -20,7 +20,6 @@ function updateProfileInfo(profileData){
     email.innerText = profileData.email
 }
 
-
 function updateSoftSkills(profileData){
     const softSkills = document.getElementById("profile.skills.softSkills")
     softSkills.innerHTML = profileData.skills.softSkills.map(skill =>`<li>${skill}</li>`).join("")
